bfd5b268f6eb58324843343260d59045f79ebbf9
rodolfotadeu <rodolfotadeu8@gmail.com>
2024-05-16 22:01:01 -0300

    Add a new props setErrorMessage

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -5,12 +5,24 @@ import { CustomInput } from "../CustomInput/CustomInput";
 import "./AddTask.scss";
 import { CustomButton } from "../CustomButton/CustomButton";
 
-export const AddTask = () => {
+export const AddTask = ({ setErrorMessage }) => {
   const [task, setTask] = useState("");
 
   const onChange = (e) => {
     setTask(e.target.value);
+    setErrorMessage("");
   };
+
+  const handleTaskAddition = async () => {
+    try {
+      if (task.length === 0) {
+        setErrorMessage(
+          "A tarefa precisa de uma descrição para ser adicionada"
+        );
+      }
+    } catch (error) {}
+  };
+
   return (
     <div className="add-task-container">
       <CustomInput
@@ -18,7 +30,7 @@ export const AddTask = () => {
         value={task}
         onChange={onChange}
       />
-      <CustomButton>
+      <CustomButton onClick={handleTaskAddition}>
         <FaPlus size={14} color="#ffffff" />
       </CustomButton>
     </div>
