2a4a041b4861f95bb5e7de5cec6a8b6aa5102a80
rodolfotadeu <rodolfotadeu8@gmail.com>
2024-05-17 20:33:31 -0300

    feat: add onEnterPress prop

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,12 +1,29 @@
 import "./CustomInput.scss";
 
-export const CustomInput = ({ label, value, onChange }) => {
+export const CustomInput = ({
+  label,
+  value,
+  onChange,
+  onEnterPress,
+  setErrorMessage,
+}) => {
+  console.log("Value", value.length);
+  const handleKeyDown = (e) => {
+    if (value.length > 0 && e.key === "Enter") {
+      onEnterPress();
+    } else {
+      setErrorMessage("A tarefa precisa de uma descrição para ser adicionada");
+    }
+  };
+
   return (
     <div className="custom-input-container">
       <input
+        type="text"
         className="custom-input"
-        onChange={(e) => onChange(e)}
         value={value}
+        onChange={(e) => onChange(e)}
+        onKeyDown={(e) => handleKeyDown(e)}
       />
       {label ? (
         <label
