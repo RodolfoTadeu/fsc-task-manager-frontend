7c2f1a1c1c8ca01afc79ddbd24422d1ca0cd8103
rodolfotadeu <rodolfotadeu8@gmail.com>
2024-05-19 17:19:17 -0300

    feat: add app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import "./App.scss";
+
+import { Sidebar } from "./components/Sidebar/Sidebar";
 import { Task } from "./components/Tasks/Tasks";
 
 const App = () => {
   return (
-    <>
+    <div className="app-container">
+      <Sidebar />
       <Task />
-    </>
+    </div>
   );
 };
 
